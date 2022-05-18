5f49a5756cf235a40bb3f744287e4fd57e33950d
Rui Zhu <kevinricky121@gmail.com>
2022-05-18 15:27:28 +0200

    Sign up page

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyDrcSDXbQ0X6kHmmZ1wQS3_YA_hhfWTUL8",
@@ -17,8 +19,9 @@ firebase.initializeApp(firebaseConfig)
 //init services
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
 
 //timestamp
 const timestamp = firebase.firestore.Timestamp
 
-export { projectFirestore, projectAuth, timestamp }
+export { projectFirestore, projectAuth, projectStorage, timestamp }
