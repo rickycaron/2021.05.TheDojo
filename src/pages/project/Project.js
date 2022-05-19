e40b1b171621c74868439b1c6e98d570621a1441
Rui Zhu <kevinricky121@gmail.com>
2022-05-19 15:53:07 +0200

    project page

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,7 +1,25 @@
+import { useParams } from "react-router-dom"
+import { useDocument } from '../../hooks/useDocument'
+
+// styles
 import './Project.css'
 
 export default function Project() {
+    const { id } = useParams()//get the id of the document from the url
+    const { document, error } = useDocument('projects', id)
+
+    if (error) {
+        //if there is an error, just return the error and ignore all the contents below
+        return <div className="error">{error}</div>
+    }
+    if (!document) {
+        //while the document is not finsihed, return the loading message, when the ducument returns, the below return will run
+        return <div className="loading">Loading...</div>
+    }
+
     return (
-        <div>Project</div>
+        <div className="project-details">
+            <h1>{document.name}</h1>
+        </div>
     )
 }
