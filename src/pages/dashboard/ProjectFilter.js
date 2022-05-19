46f687ac1f3f0cd037f20b98209a8fcdd2e7d0e7
Rui Zhu <kevinricky121@gmail.com>
2022-05-19 20:30:28 +0200

    filter project by category

--- src/pages/dashboard/ProjectFilter.js
+++ src/pages/dashboard/ProjectFilter.js
@@ -3,24 +3,24 @@ import { useState } from 'react'
 const filterList = ['all', 'mine', 'development', 'design', 'marketing', 'sales']
 
 export default function ProjectFilter({ changeFilter }) {
-  const [currentFilter, setCurrentFilter] = useState('all')
+    const [currentFilter, setCurrentFilter] = useState('all')
 
-  const handleClick = (newFilter) => {
-    setCurrentFilter(newFilter)
-    changeFilter(newFilter)
-  }
+    const handleClick = (newFilter) => {
+        setCurrentFilter(newFilter)
+        changeFilter(newFilter)
+    }
 
-  return (
-    <div className="project-filter">
-      <nav>
-        <p>Filter by: </p>
-        {filterList.map((f) => (
-          <button key={f}
-            onClick={() => handleClick(f)}
-            className={currentFilter === f ? 'active' : ''}
-          >{f}</button>
-        ))}
-      </nav>
-    </div>
-  )
+    return (
+        <div className="project-filter">
+            <nav>
+                <p>Filter by: </p>
+                {filterList.map((f) => (
+                    <button key={f}
+                        onClick={() => handleClick(f)}
+                        className={currentFilter === f ? 'active' : ''}
+                    >{f}</button>
+                ))}
+            </nav>
+        </div>
+    )
 }
